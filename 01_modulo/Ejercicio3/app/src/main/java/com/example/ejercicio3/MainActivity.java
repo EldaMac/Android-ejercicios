7127cc69521998354038b7b24b5f050084a5faf0
Elda <elda22020@gmil.com>
2019-05-15 12:33:27 -0500

    Este es el ejercicio de la suma y resta con radio button
    Ejercicio 3 operaciones con Radio Button

--- 01_modulo/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.java
+++ 01_modulo/Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.ejercicio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText varTxtNum1;
+    EditText varTxtNum2;
+    RadioButton varRdSuma;
+    RadioButton varRdResta;
+    Button varButton;
+    TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        varTxtNum1 = (EditText) findViewById(R.id.txtNum1);
+        varTxtNum2 = (EditText) findViewById(R.id.txtNum2);
+
+        varRdSuma = (RadioButton) findViewById(R.id.rdSuma);
+        varRdResta = (RadioButton) findViewById(R.id.rdResta);
+
+        varButton = (Button) findViewById(R.id.btnOperacion);
+        txtResultado = (TextView) findViewById(R.id.txtResultado);
+
+        varButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int resultado = 0;
+                int num1 = convertiraEntero(varTxtNum1.getText().toString());
+                int num2 =  convertiraEntero(varTxtNum2.getText().toString());
+                if(varRdSuma.isChecked())
+                    resultado = num1 + num2;
+                else
+                    resultado = num1 - num2;
+                txtResultado.setText("El resultado es " + resultado);
+
+            }
+        });
+
+    }
+
+    public int convertiraEntero (String cadena){
+        try {
+          return Integer.parseInt(cadena);
+        }catch (NumberFormatException e){
+            return 0;
+        }
     }
 }
